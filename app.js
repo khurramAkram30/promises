f16e189199e27d021c8672722040cf3407b23afb
khurram.Akram30 <khurram.akram43@gmail.com>
2018-04-18 00:27:56 -0700

    promises

--- app.js
+++ app.js
@@ -13,16 +13,69 @@
 //      }
 //      }
 //      }
-    
 
-function greetWorld() {
-    try {
-     var greeting = "Hello world!";
-     alert(greeting);
-     }
-     catch(ex) {
-     alert(ex);
-    }
-     }
 
-     greetWorld();
+// function greetWorld() {
+//     try {
+//      var greeting = "Hello world!";
+//      alert(greeting);
+//      }
+//      catch(ex) {
+//      alert(ex);
+//     }
+//      }
+
+//      greetWorld();
+
+// setTimeout(function (){
+//     console.log("first");
+// },1000);
+// console.log("time");
+
+// setTimeout(function (){
+//     console.log("second");
+// },1000);
+
+// setTimeout(function (){
+//     console.log("third");
+// },1000);
+
+
+// var i=0;
+// function showConfirm(){
+//     return new Promise(function (resolve,reject){
+
+//         setTimeout(function(){
+//             console.log('second',i++);
+//             resolve();
+//         },1000);
+//     })
+// }
+// showConfirm()
+// .then(function(){
+//     console.log("settimeout finished");
+// })
+
+
+var i = 0;
+
+function showConfirm(num) {
+    return new Promise(function (resolve, reject) {
+        var random = Math.floor(Math.random() * 11);
+
+        if(num/random % 2 === 0){
+            console.log("inside promise if",random);
+            var obj={exBlock:"inside if", flagBool:(num/random % 2 === 0)}
+        resolve(obj);
+        }
+        else{
+        console.log("inside promise else",random);
+        var obj={exBlock:"inside else", flagBool:(num/random % 2 === 0)}
+        resolve(obj);
+        }
+    })
+}
+showConfirm(6)
+    .then(function (obj) {
+        console.log(obj);
+    })
